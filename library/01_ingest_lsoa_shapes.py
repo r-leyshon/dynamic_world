9f4a5ea1c888e66be003a1cca078b396407159ae
r-leyshon <richard.leyshon@ons.gov.uk>
2022-07-16 13:26:00 +0100

    feat: Paginated ingestion of 2011 generalised LSOA shapes

--- library/01_ingest_lsoa_shapes.py
+++ library/01_ingest_lsoa_shapes.py
@@ -2,4 +2,15 @@
 
 from src.make_data.ingest_ONS_geo import get_shapes
 
-get_shapes()
+# this resource is limited to 50 records at a time, pagination required
+cont_loop = True
+resp_strings = []
+offset = 0
+
+while cont_loop:
+    cont = get_shapes(params={"resultOffset": offset, "resultRecordCount": 50})
+    resp_strings.append(cont)
+    # Update the offset to scroll through 50 records
+    offset += 50
+    print(f"next record batch starts at {offset}")
+    cont_loop = "exceededTransferLimit" in cont
--- src/make_data/ingest_ONS_geo.py
+++ src/make_data/ingest_ONS_geo.py
@@ -3,24 +3,49 @@
 import toml
 from pyprojroot import here
 import os
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+import requests
 
 
 def get_shapes(
     endpoint=toml.load(os.path.join(here(), "library", "01_ingest_lsoa_shapes.toml"))[
         "ONS_GEO"
     ]["LSOA_ENDPOINT"],
-    retry_strategy=None,
+    retry_strategy=Retry(
+        total=3,
+        status_forcelist=[429, 500, 502, 503, 504],
+        allowed_methods=["HEAD", "GET", "OPTIONS"],
+        raise_on_status=False,
+        backoff_factor=1,
+    ),
     headers={
         "Accept": "application/json",
         "user-agent": toml.load(os.path.join(here(), ".secrets.toml"))["REMOTES"][
             "USER_AGENT"
         ],
     },
+    params={"resultOffset": 0, "resultRecordCount": 50},
 ):
     """
     Request the LSOA shapefiles from ONS geoportal
     """
+    # prep requests session
+    resource_adapter = HTTPAdapter(max_retries=retry_strategy)
+    req_session = requests.Session()
+    req_session.mount("https://", resource_adapter)
+    req_session.mount("http://", resource_adapter)
+    req_session.headers.update(headers)
+    resp = req_session.get(endpoint, params=params)
+    # handle the response
+    if resp.ok is not True:
+        raise ValueError(f"Response returned {resp.status_code}.")
 
-    print(endpoint)
+    elif "application/json" not in resp.headers["Content-Type"]:
+        raise ValueError(
+            f"Response not as expected, {resp.headers.get('content-type')} returned."
+        )
 
-    print(headers)
+    else:
+        content = resp.json()
+        return content
